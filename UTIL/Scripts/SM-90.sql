2f6b6dafa2b9290fc518c7fa1200a45155199327
Yollcheck <64315404+Yollcheck@users.noreply.github.com>
2022-06-23 22:07:21 +0200

    Update SM-90.sql

--- UTIL/Scripts/SM-90.sql
+++ UTIL/Scripts/SM-90.sql
@@ -2,4 +2,4 @@ CREATE INDEX sys_idx ON util.sys_log (LOG_DATE) GLOBAL;
 
 CREATE INDEX cur_hist_idx ON util.cur_exch_rate_history (CURR_CODE) GLOBAL;
 
-CREATE INDEX cur_exch_idx ON util.cur_exch_rate (EXCHANGEDATE) GLOBAL;
+CREATE INDEX CUR_HIST_DATE_IDX ON util.cur_exch_rate_history (EXCHANGEDATE) GLOBAL;
